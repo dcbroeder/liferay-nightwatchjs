e495c3dafa6f0dc80bb9a56d534bdd3d21b3b4a9
Daniel Carrillo Broeder <dcarrillo@minsait.com>
2020-03-02 16:55:58 +0100

    Clean

--- nightwatch-lrworkspace/tests/nightwatch/commands/liferayLogout.js
+++
@@ -1,6 +0,0 @@
-exports.command = function(server, username, password) {
-    var self = this;
-    return self.url(server+"/c/portal/logout")
-      .waitForElementVisible("#p_p_id_com_liferay_product_navigation_user_personal_bar_web_portlet_ProductNavigationUserPersonalBarPortlet_")
-      .assert.visible("#p_p_id_com_liferay_product_navigation_user_personal_bar_web_portlet_ProductNavigationUserPersonalBarPortlet_ .sign-in");
-};
